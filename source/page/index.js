64351f28d70e8e243e4f6461823ee30f5cdb8149
Atheer <atheerwaled66@hotmail.se>
2022-04-27 14:19:11 +0200

    implement input to sound pipeline
    now the piano keys can play the sound of the specified function
    
    Co-Authored-By: Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>

--- source/page/index.js
+++ source/page/index.js
@@ -1,3 +1,7 @@
+
+var wf = new WaveForm();
+var parser = new MathParser("t");
+
 /* GET input value, attach id to input field. If input field = input do ".value"; else if div do ".innerHTML"*/ 
 function getFunction() {
     return document.getElementById("functionInput").value;
@@ -19,6 +23,24 @@ function startNote(note){
 function stopNote(note){
     //Insert management for stopping a note
     console.log(note + " stoppades");
+}
+
+function startNote(note){
+	console.log(note + " startades");
+	var str_fn = getFunction();
+	var fn = parser.parse(str_fn);
+	//console.log(note, fn);
+	wf.genBufferFromNote(fn, note);
+	//wf.normalizeBuffer();
+	wf.fadeOutEnd(2000);
+	wf.playBuffer();
+}
+
+function stopNote(note){
+	console.log(note + " stoppades");
+	//wf.stopBuffer();
+	//console.log("end1", wf.masterSource.getChannelDate);
+	//console.log("end2", wf.audioContext.getOutputTimestamp());
 }
 
 //createPiano();
