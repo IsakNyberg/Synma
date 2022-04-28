1ecb528f1cb6c586a92c2cba104efc8634e4b6ef
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2022-04-28 09:37:14 +0200

    improve graph code structure; add comments
    Co-Authored-By: AzerHojlas <80171742+AzerHojlas@users.noreply.github.com>

--- source/page/graph.js
+++ source/page/graph.js
@@ -1,3 +1,8 @@
+
+/* 
+ Takes the number of points and the max values and returns an array of length 
+ numPoints that are evenly spaced and below the max value.
+*/
 function makeDomain(numPoints, maxX) {
 	var step = maxX / numPoints;
 	var domain = Array(numPoints);
@@ -9,6 +14,10 @@ function makeDomain(numPoints, maxX) {
 	return domain;
 }
 
+/* 
+ Takes each entry in the domain and applies the function on it and returns and 
+ array of theses values.
+*/
 function makeRange(fn, domain){
 	var range = Array(domain.length);
 	for (var i = 0; i < domain.length; i++) {
@@ -17,6 +26,11 @@ function makeRange(fn, domain){
 	return range;
 }
 
+/* 
+ Takes each entry in the domain and applies the function on it and returns and 
+ array of theses values. These values are capped at -1 and +1 so that values 
+ above and below are not graphed.
+*/
 function makeNormalizedRange(fn, domain){
 	var range = Array(domain.length);
 	for (var i = 0; i < domain.length; i++) {
@@ -24,9 +38,15 @@ function makeNormalizedRange(fn, domain){
 	}
 	return range;
 }
-
-function display(ctx, fn, numPoints, maxX, normalize) {
-	console.log(ctx);
+/**
+ * Receives domain and range from separate functions.
+ * Depending on if the client wishes the graph to be normalized, i.e within 
+ * range [-1, 1], the boolean normalize 
+ * indicates the proper range.
+ * Once the domain and range have been calculated, they are returned as an 
+ * array, whish is used when drawing the function
+ */
+function makeDatapoints(fn, numPoints, maxX, normalize) {
 	var domain = makeDomain(numPoints, maxX);
 	var range;
 	if (normalize) {
@@ -34,42 +54,57 @@ function display(ctx, fn, numPoints, maxX, normalize) {
 	} else {
 		range = makeRange(fn, domain);
 	}
-	window.onload = function() {drawGraph(domain, range)}
-	
-	function drawGraph(domain, range){
-		var data = {
-			labels: domain, // x-axeln
-				datasets: [{
-					function: function(x) { return eval(fn)},  // Omvandlar sträng till javascript kod
-					borderColor: "rgba(255, 206, 86, 1)",   // Färgspektrum
-					data: range,   // y-axeln i vanliga fall, men i detta fall skriver man en funktion
-					fill: false
-				}]
-		};
-		
-		// Configuration of chart
-		const config = {
-			type: 'line',
-			data: data,
-			options: {	 
-				legend: {
-					display: false
-				},
-				scales: {
-					xAxes: [{
-						display: false
-					}],
-					yAxes: [{
-						ticks: {
-							min: -1,
-							max: +1,
-							beginAtZero:true
-						}
-					}]    
-				},
-			}
-		};
-		
-		return new Chart(ctx, config);
-	}
+  return [domain, range];	
+}
+
+/**
+ * Uses the Chart JS library, which receives a dataset that shapes the function
+ * and chart points, e.g: range, domain and colouring; 
+ * and a configuration that provides the esthetics of the chart, eg: scale, 
+ * axis, display etc
+ */
+function drawDomainRange(ctx, domain, range){
+  var data = {
+    labels: domain, // x-axeln
+    datasets: [{
+      borderColor: "rgba(255, 206, 86, 1)",   // Färgspektrum
+      data: range,   // y-axeln
+      fill: false,
+    }]
+  };
+  // Configuration of chart
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          display: false
+        }],
+        yAxes: [{
+          ticks: {
+            min: -1,
+            max: +1,
+            beginAtZero:true
+          }
+        }]    
+      },
+    }
+  };
+  new Chart(ctx, config);
+}
+
+/** 
+  Does everything needed to draw a graph in the context canvas 
+  @param fn is the function to graph
+  @param numPoints is the number of datapoints to be plotted
+  @param maxX is the largest value that x will take
+  @param normalize boolean that determines if the values are capped at ±1
+*/
+function drawGraph(ctx, fn, numPoints, maxX, normalize){
+  var [domain, range] = makeDatapoints(fn, numPoints, maxX, normalize);
+  drawDomainRange(ctx, domain, range);
 }
