14c01e96117d9eee09f1a606bdc2e1066eec64dc
rasdan <rasdan@kth.se>
2022-05-12 15:39:02 +0200

    init commit, start on switch to iFrame

--- source/page/index.js
+++ source/page/index.js
@@ -1,3 +1,4 @@
+var documents = [document];
 window.onload = init_synt();
 /**
  * Program start. Init. the synth.
@@ -6,68 +7,68 @@ function init_synt(){
   var synth;
 	let origin = window.location.search;
 	const urlParams = new URLSearchParams(origin);
-	if(urlParams.has('func1')){
-        synth = new Synth(loadURL(urlParams),document.getElementById('waveformGraph'),document.getElementById('envelopeGraph'));
-        
-        
-	}
-	else{
-		synth = new Synth([],document.getElementById('waveformGraph'),document.getElementById('envelopeGraph'));
-	}
+	synth = urlParams.has('func1') ?
+        new Synth(loadURL(urlParams),document.getElementById('waveformGraph'),document.getElementById('envelopeGraph')):
+        new Synth([],document.getElementById('waveformGraph'),document.getElementById('envelopeGraph'));
     dropdownClick(synth);
     addEventSynthListeners(synth);
 	const midiKeybaord = new MidiKeybaord(synth, synth.piano);
 }
-function addEventSynthListeners(synth){
-    document.getElementById("saveSettings").onclick = () => synth.saveSettings();
-    document.getElementById("recordButton").onclick = () => {
-        let recordButtonElement = document.getElementById("recordButton");
-        let playButtonElement = document.getElementById("playButton");
-        let downloadButtonElement = document.getElementById("downloadButton");
+function addMainEventListeners() {
+    document.addEventListener().onclick = () => {
+        documents.push(DOMImplementation.createHTMLDocument("PLACEHOLDER"))
+    };
+}
+function addEventSynthListeners(synth, doc){
+    doc.getElementById("saveSettings").onclick = () => synth.saveSettings();
+    doc.getElementById("recordButton").onclick = () => {
+        let recordButtonElement = doc.getElementById("recordButton");
+        let playButtonElement = doc.getElementById("playButton");
+        let downloadButtonElement = doc.getElementById("downloadButton");
         synth.recorder(recordButtonElement,playButtonElement,downloadButtonElement);
     };
-    document.getElementById("playButton").onclick = () => {
-        let playButtonElement = document.getElementById("playButton");
+    doc.getElementById("playButton").onclick = () => {
+        let playButtonElement = doc.getElementById("playButton");
         synth.player(playButtonElement);
     };
-    document.getElementById("functionButton").onclick = () => {
-        let fnString = document.getElementById("functionInput").value;
-        let normalized = document.getElementById("normalizeCheckbox").checked;
-        let maxXInput = document.getElementById("maxXInput").value;
+    doc.getElementById("functionButton").onclick = () => {
+        let fnString = doc.getElementById("functionInput").value;
+        let normalized = doc.getElementById("normalizeCheckbox").checked;
+        let maxXInput = doc.getElementById("maxXInput").value;
         synth.setWave(fnString,normalized,maxXInput);
     };
-    document.getElementById("env-functionButton").onclick = () =>{ 
-        let envelopString = document.getElementById("chosenEnvelope").innerHTML.toLowerCase();
-		let envelopMaxT = document.getElementById("chosenTimezone").innerHTML.toLowerCase();
-		let fnString = document.getElementById("env-functionInput").value;
-		let timeString = document.getElementById("env-timeInput").value;
-        let normalized = document.getElementById("normalizeEnvelope").checked;
-        let continuous = document.getElementById("continuousCheckbox").checked;
+    doc.getElementById("env-functionButton").onclick = () =>{ 
+        let envelopString = doc.getElementById("chosenEnvelope").innerHTML.toLowerCase();
+		let envelopMaxT = doc.getElementById("chosenTimezone").innerHTML.toLowerCase();
+		let fnString = doc.getElementById("env-functionInput").value;
+		let timeString = doc.getElementById("env-timeInput").value;
+        let normalized = doc.getElementById("normalizeEnvelope").checked;
+        let continuous = doc.getElementById("continuousCheckbox").checked;
         synth.setEnvelopes(envelopString,envelopMaxT,fnString,timeString, normalized, continuous);
     };
-    document.getElementById("applyAmplitude").onclick = (clicked) => {
-        synth.activeEnvelopes[0]=document.getElementById("applyAmplitude").checked;
+    doc.getElementById("applyAmplitude").onclick = (clicked) => {
+        synth.activeEnvelopes[0]=doc.getElementById("applyAmplitude").checked;
     } 
-    document.getElementById("applyPitch").onclick = (clicked) => {
-        synth.activeEnvelopes[1]=document.getElementById("applyPitch").checked;
+    doc.getElementById("applyPitch").onclick = (clicked) => {
+        synth.activeEnvelopes[1]=doc.getElementById("applyPitch").checked;
     } 
-    document.getElementById("applyFilter").onclick = (clicked) => {
-        synth.activeEnvelopes[2]=document.getElementById("applyFilter").checked;
+    doc.getElementById("applyFilter").onclick = (clicked) => {
+        synth.activeEnvelopes[2]=doc.getElementById("applyFilter").checked;
     } 
-    document.getElementById("volume").oninput = () => synth.setMasterVolume(document.getElementById("volume").value);
-    document.querySelectorAll(".dropdownOption").forEach(
+    doc.getElementById("volume").oninput = () => synth.setMasterVolume(doc.getElementById("volume").value);
+    doc.querySelectorAll(".dropdownOption").forEach(
         (element)=> {element.addEventListener("click", ()=> dropdownClick(synth))}
     );
-    document.getElementById("midiUpload").addEventListener("change", ()=>{
-        synth.playFile(document.getElementById("midiUpload").files[0]);
+    doc.getElementById("midiUpload").addEventListener("change", ()=>{
+        synth.playFile(doc.getElementById("midiUpload").files[0]);
     }, false);
 }
 function dropdownClick(synth){
-    var currentType = document.getElementById("chosenEnvelope").innerHTML.toLowerCase();
-    var currentTime = document.getElementById("chosenTimezone").innerHTML.toLowerCase();
-    document.getElementById("env-functionInput").value = synth.envFunctions[currentType][currentTime][1];
-    document.getElementById("env-timeInput").value = synth.envFunctions[currentType][currentTime][2];
-    document.getElementById("normalizeEnvelope").checked = synth.envIsNormalized[currentType][0];
-    document.getElementById("continuousCheckbox").checked = synth.envIsNormalized[currentType][1];
+    var currentType = doc.getElementById("chosenEnvelope").innerHTML.toLowerCase();
+    var currentTime = doc.getElementById("chosenTimezone").innerHTML.toLowerCase();
+    doc.getElementById("env-functionInput").value = synth.envFunctions[currentType][currentTime][1];
+    doc.getElementById("env-timeInput").value = synth.envFunctions[currentType][currentTime][2];
+    doc.getElementById("normalizeEnvelope").checked = synth.envIsNormalized[currentType][0];
+    doc.getElementById("continuousCheckbox").checked = synth.envIsNormalized[currentType][1];
     synth.graphEnvelope(currentType);
 }
