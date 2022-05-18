e5dcb62a263bd1922aab8414cf3034433cf76228
hasan <97052700+HasanAlzubeidi@users.noreply.github.com>
2022-05-18 13:15:58 +0200

    Remove unused functions

--- source/page/roll/pianoroll.js
+++ source/page/roll/pianoroll.js
@@ -70,25 +70,25 @@ class Rectangle{
 	 this.x1 += move;
 	}
 
-	setX1(xxxxxxxx){
-		this.x1 = xxxxxxxx;
+	setX1(length){   //xxxxxxxx to x1
+		this.x1 = length;
 	}
 
-	check(x1000,y1000){
-		if(x1000 >= this.x && x1000 <= (this.x + 19) && y1000 >= this.y && y1000 <= (this.y+19))
-			return true;
-	}
+	// check(x1000,y1000){
+	// 	if(x1000 >= this.x && x1000 <= (this.x + 19) && y1000 >= this.y && y1000 <= (this.y+19))
+	// 		return true;
+	// }
 
-	increaseIndex()
-	{
-		let bbb = this.getX1()/20;
-		return bbb;
-	}
+	// increaseIndex()
+	// {
+	// 	let bbb = this.getX1()/20;
+	// 	return bbb;
+	// }
 }
 
 const Rectangles = [];
 const Rectangles1 = [];
-const Rectangles2 = [];
+//const Rectangles2 = [];
 const smallRectangles = [];
 const timeline = [new Rectangle(
 	0,
@@ -98,7 +98,7 @@ const timeline = [new Rectangle(
 	2560,
 	)
 ];
-let redRectangles = [];
+//let redRectangles = [];
 let x = 0;
 let y = 0;
 let y1=0;
@@ -152,13 +152,13 @@ function animate(){
 	Rectangles1.forEach((Rectangle)=>{
 		Rectangle.update();
 	});
-	redRectangles=[];
+	//redRectangles=[];
 	smallRectangles.forEach((Rectangleq)=>{
 		Rectangleq.update();
 	});
-	redRectangles.forEach((Rectangle)=>{
-		Rectangle.update();
-	})
+	// redRectangles.forEach((Rectangle)=>{  
+	// 	Rectangle.update();
+	// })
 	timeline.forEach((Rectangle)=>{
 		Rectangle.update();
 	})
